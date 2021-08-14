e93cbee16adbbf6129920767033b7922fca2a429
Song-EunJu <jd06280@naver.com>
2021-08-14 15:36:25 +0900

    [ADD] 하단 네비게이션바 추가

--- app/src/main/java/com/example/metootravel/MainActivity.java
+++ app/src/main/java/com/example/metootravel/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.metootravel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
+
+    Fragment homeFragment;
+    Fragment messageFragment;
+    Fragment letterFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_letter, )
+
+            case R.id.action_message:
+
+            case R.id.action_letter:
+
+        }
+        return false;
     }
 }
