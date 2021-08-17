51ecb7406f1dc4460bd9f025b036f37c661f1483
Song-EunJu <jd06280@naver.com>
2021-08-17 18:29:25 +0900

    [EDIT] 내비게이션 함수 수정

--- app/src/main/java/com/example/metootravel/MainActivity.java
+++ app/src/main/java/com/example/metootravel/MainActivity.java
@@ -9,36 +9,63 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.example.metootravel.navigation.LetterFragment;
+import com.example.metootravel.navigation.MessageFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 import org.jetbrains.annotations.NotNull;
 
-public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity {
 
-    Fragment homeFragment;
     Fragment messageFragment;
     Fragment letterFragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener m = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+            switch (item.getItemId()) {
+//            case R.id.action_home:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_letter, )
+
+                case R.id.action_message:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_content, messageFragment).commit();
+                    return true;
+                case R.id.action_letter:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_content, letterFragment).commit();
+                    return true;
+
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        NavigationBarView navbar = findViewById(R.id.navigation_bar); // activity_main 에 네비게이션 바 id
+        navbar.setOnItemSelectedListener(m);
+        messageFragment = new MessageFragment();
+        letterFragment = new LetterFragment();
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
-        switch(item.getItemId()){
-            case R.id.action_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_letter, )
-
-            case R.id.action_message:
-
-            case R.id.action_letter:
-
-        }
-        return false;
-    }
 }
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+//        switch(item.getItemId()){
+////            case R.id.action_home:
+////                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_letter, )
+//
+//            case R.id.action_message:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.main_content, messageFragment).commit();
+//                return true;
+//            case R.id.action_letter:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.main_content, letterFragment).commit();
+//                return true;
+//
+//        }
+//        return false;
+//    }
