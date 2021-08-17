dfa9181e8883a2f59ef717bb4928e8095fa5fd69
Song-EunJu <jd06280@naver.com>
2021-08-17 20:48:57 +0900

    [EDIT] Fragment 화면 이동

--- app/src/main/java/com/example/metootravel/MainActivity.java
+++ app/src/main/java/com/example/metootravel/MainActivity.java
@@ -23,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
     Fragment messageFragment;
     Fragment letterFragment;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener m = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private BottomNavigationView.OnNavigationItemSelectedListener m
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
         @Override
-        public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
 //            case R.id.action_home:
 //                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_letter, )
@@ -46,26 +48,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NavigationBarView navbar = findViewById(R.id.navigation_bar); // activity_main 에 네비게이션 바 id
-        navbar.setOnItemSelectedListener(m);
+        BottomNavigationView nav = findViewById(R.id.navigation_bar); // activity_main 에 네비게이션 바 id
+        nav.setOnNavigationItemSelectedListener(m);
+//        NavigationBarView navbar = findViewById(R.id.navigation_bar); // activity_main 에 네비게이션 바 id
+//        navbar.setOnItemSelectedListener(m);
         messageFragment = new MessageFragment();
         letterFragment = new LetterFragment();
     }
 
 }
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
-//        switch(item.getItemId()){
-////            case R.id.action_home:
-////                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_letter, )
-//
-//            case R.id.action_message:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.main_content, messageFragment).commit();
-//                return true;
-//            case R.id.action_letter:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.main_content, letterFragment).commit();
-//                return true;
-//
-//        }
-//        return false;
-//    }
